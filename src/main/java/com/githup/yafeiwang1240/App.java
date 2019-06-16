5cffbbb0779c4400f1afda3c1ade5ad6fb445524
wangyafei <364968487@qq.com>
2019-06-16 08:36:44 +0800

    Java Bean封装

---
+++ src/main/java/com/githup/yafeiwang1240/App.java
@@ -0,0 +1,13 @@
+package com.githup.yafeiwang1240;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
