cdaa7963ade54dbaa0600b0dd42e47108c43becb
wangyafei <364968487@qq.com>
2019-06-27 09:47:28 +0800

    新增IO阻塞方法

--- src/main/java/com/githup/yafeiwang1240/obrien/uitls/IOUtils.java
+++ src/main/java/com/githup/yafeiwang1240/obrien/uitls/IOUtils.java
@@ -7,8 +7,16 @@ public class IOUtils {
     public static void closeQuietly(Closeable o) {
         try {
             o.close();
-        } catch (IOException e) {
+        } catch (Exception e) {
+            // ignore
+        }
+    }
 
+    public static void sleep(long millis) {
+        try {
+            Thread.sleep(millis);
+        } catch (Exception e) {
+            // ignore
         }
     }
 }
